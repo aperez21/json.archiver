31e441d2e0e29cbfac2880f0252bfa34a8c026bf
aperez21 <austreberto.perez@gmail.com>
2017-01-20 21:19:34 -0600

    supporting jars

---
+++ src/org/rest/json/archive/HTTPTest.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package org.rest.json.archive;
+
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.Header;
+import org.apache.http.HeaderIterator;
+/**
+ * @author Luck
+ *
+ */
+public class HTTPTest {
+
+	/**
+	 * 
+	 */
+	public HTTPTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
